e971cdff7870fdebed1f9517a565735105085df7
shubham <chd.nonu26@gmail.com>
2020-11-21 00:11:24 +0530

    run

--- Backtracking/sudoku_solver.cpp
+++ Backtracking/sudoku_solver.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 void recursion(int arr[][],int i,int j){
     if(j==arr[0].length()-1){
-
+        i=0;j=0;
     }
     else {
-
+        i=i;
+        j+=1;
     }
     if(arr[i][j]!=0){
         recursion(arr,i,j);
